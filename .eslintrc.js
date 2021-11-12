04ad888230a1c61037364ea206b9c26ce578d082
Min Nam <code.minnam@gmail.com>
2021-11-12 23:53:29 +0000

    Add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'react'],
+  rules: {},
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const App = () => {
-  return (
-    <div className="App">
-      Hello Athena!
-    </div>
-  );
-}
+  return <div className="App">Hello Athena!</div>;
+};
 
 export default App;
