a3bccd02b583a91443a88f6fdd74b9266c56b2ec
grace <telegrace@gmx.de>
2021-11-14 00:35:53 +0100

    feat: background added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import TaskManager from './components/task-manager';
+import TaskManager from './components/taskManager/index';
 import MusicPlayer from './components/MusicPlayer';
 
 const App = () => {
   return (
     <div className="App">
-      Hello Athena!
-      <TaskManager />
-      <MusicPlayer />
+      <div className="left-hand-side">
+        <div className="weather-time-container">Hello Athena!</div>
+        <div className="task-manager-container">
+          <TaskManager />
+        </div>
+      </div>
+      <div className="widgets-container">
+        <MusicPlayer />
+      </div>
     </div>
   );
 };
