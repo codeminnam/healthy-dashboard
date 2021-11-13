ac84e024cec9ce2177ce51f914b475b01996a8be
grace <telegrace@gmx.de>
2021-11-13 14:42:00 +0100

    init

---
+++ src/api/WeatherComponent.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import axios from 'axios';
+
+interface WeatherComponentProps {
+  props: unknown;
+}
+
+const WeatherComponent: React.FC<WeatherComponentProps> = (props) => {
+  console.log(props);
+  return <></>;
+};
+
+export default WeatherComponent;
